c17cfa26192e6d50292420e2f9bfd46a339eb8f8
mustafa1912 <80777113+mustafa1912@users.noreply.github.com>
2021-10-22 02:55:07 +0200

    Update app.js

--- js/app.js
+++ js/app.js
@@ -1,115 +1,76 @@
 /* Start nav */
-let nav = document.getElementById("nav");
+//create nav
 
+let nav = document.getElementById("nav");
 for (var i = 1; i <= 4; i++) {
     let newLi = document.createElement("li");
     newLi.innerHTML = " <a>Section </a> " + i;
     nav.appendChild(newLi);
 }
 
+//scroll
 let Li = document.getElementsByTagName("Li");
-
-let section_1 = document.getElementById("section_1");
-let section_2 = document.getElementById("section_2");
-let section_3 = document.getElementById("section_3");
-let section_4 = document.getElementById("section_4");
-
-Li[0].addEventListener('click', funSection_1);
-
-function funSection_1() {
-    section_1.scrollIntoView();
-    Li[0].classList.add('active_nav');
-    Li[2].classList.remove('active_nav');
-    Li[1].classList.remove('active_nav');
-    Li[3].classList.remove('active_nav');
+let section = document.querySelectorAll('.section');
 
 
-    section_1.classList.add('active');
-    section_2.classList.remove('active');
-    section_3.classList.remove('active');
-    section_4.classList.remove('active');
-}
+Li[0].addEventListener('click', () => {
+    section[0].scrollIntoView();
+});
 
-Li[1].addEventListener('click', funSection_2);
+Li[1].addEventListener('click', () => {
+    section[1].scrollIntoView();
+});
 
-function funSection_2() {
-    section_2.scrollIntoView();
-    Li[1].classList.add('active_nav');
-    Li[2].classList.remove('active_nav');
-    Li[3].classList.remove('active_nav');
-    Li[0].classList.remove('active_nav');
+Li[2].addEventListener('click', () => {
+    section[2].scrollIntoView();
+});
 
-    section_2.classList.add('active');
-    section_1.classList.remove('active');
-    section_3.classList.remove('active');
-    section_4.classList.remove('active');
-}
+Li[3].addEventListener('click', () => {
+    section[3].scrollIntoView();
+});
 
-Li[2].addEventListener('click', funSection_3);
+let sections = document.getElementsByTagName('section');
+onscroll = () => {
+    let scrollPosation = document.documentElement.scrollTop;
 
-function funSection_3() {
-    section_3.scrollIntoView();
-    Li[2].classList.add('active_nav');
-    Li[3].classList.remove('active_nav');
-    Li[1].classList.remove('active_nav');
-    Li[0].classList.remove('active_nav');
+    for (var i = 0; i <= 3; i++) {
+        if (scrollPosation > sections[i].offsetTop &&
+            scrollPosation < (sections[i].offsetTop + sections[i].clientHeight)) {
 
-    section_3.classList.add('active');
-    section_1.classList.remove('active');
-    section_2.classList.remove('active');
-    section_4.classList.remove('active');
-}
+            section[i].classList.add('active');
+            section[i].classList.remove('disactive');
+            Li[i].classList.add('active_nav');
 
-Li[3].addEventListener('click', funSection_4);
-
-function funSection_4() {
-    section_4.scrollIntoView();
-
-    Li[3].classList.add('active_nav');
-    Li[2].classList.remove('active_nav');
-    Li[1].classList.remove('active_nav');
-    Li[0].classList.remove('active_nav');
-
-    section_4.classList.add('active');
-    section_1.classList.remove('active');
-    section_2.classList.remove('active');
-    section_3.classList.remove('active');
-
+        } else {
+            section[i].classList.add('disactive');
+            section[i].classList.remove('active');
+            Li[i].classList.remove('active_nav');
+        }
+    }
 }
 
 /* End nav */
 
-
 /* Start mobile menu */
 let mobileMenu = document.getElementById("mobile_menu");
-
-mobileMenu.addEventListener("click", menu);
-
-function menu() {
+mobileMenu.addEventListener("click", () => {
     mobileMenu.classList.toggle("open");
     nav.classList.toggle("mobile");
-}
-
+});
 /* End mobile menu */
-
-
-
 
 
 /* Start scroll*/
 let button = document.getElementById("button");
-window.onscroll = function() {
-    if (this.scrollY >= 100) {
+onscroll = () => {
+    if (document.documentElement.scrollTop >= 100) {
         button.classList.add("show");
     } else {
         button.classList.remove("show");
     }
 }
-
-button.addEventListener('click', click);
-
-function click() {
+button.addEventListener('click', () => {
     window.scroll(0, 0)
-}
+})
 
 /*End scroll*/
